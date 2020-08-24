45d26c777c47bb3b44863a6d5df88b0a643a9844
프로젝트 공간 <mu_2375@naver.com>
2020-08-24 13:10:17 +0900

    0824
    0824

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -3,17 +3,21 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import spring.MemberDAO;
 
 @Configuration
+@EnableTransactionManagement
 public class AppCtx {
 	
 	@Bean(destroyMethod = "close")
 	public DataSource dataSource() {
 		DataSource ds = new DataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+		ds.setUrl("jdbc:mysql://localhost/spring5fs?useSSL=false&characterEncoding=utf8");
 		ds.setUsername("spring5");
 		ds.setPassword("spring5");
 		ds.setInitialSize(2);
@@ -22,6 +26,12 @@ public class AppCtx {
 		ds.setMinEvictableIdleTimeMillis(60000 * 3);
 		ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
 		return ds;
+	}
+	
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
 	}
 	
 	@Bean
--- sp5-chap08/src/main/java/spring/MemberDAO.java
+++ sp5-chap08/src/main/java/spring/MemberDAO.java
@@ -1,14 +1,19 @@
 package spring;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.sql.Timestamp;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 
 public class MemberDAO {
 	
@@ -20,7 +25,7 @@ public class MemberDAO {
 	
 	public Member selectByEmail(String email) {
 		List<Member> results = jdbcTemplate.query(
-			"select * from MEMBER where EMAIL = ?",
+			"select * from member where EMAIL = ?",
 			new RowMapper<Member>() {
 				@Override
 				public Member mapRow(ResultSet rs, int rowNum) throws SQLException{
@@ -38,14 +43,72 @@ public class MemberDAO {
 	}
 	
 	public void insert(Member member) {
-
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update((Connection con) -> {
+			PreparedStatement pstmt = con.prepareStatement(
+				"insert into member(EMAIL, PASSWORD, NAME, REGDATE) " + 
+				"values(?, ?, ?, ?)",new String[] {" ID "});
+			pstmt.setString(1, member.getEmail());
+			pstmt.setString(2, member.getPassword());
+			pstmt.setString(3, member.getName());
+			pstmt.setTimestamp(4, Timestamp.valueOf(member.getRegisterDateTime()));
+			return pstmt;
+		},keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
 	}
 	
 	public void update(Member member) {
-
+		jdbcTemplate.update(
+			"UPDATE member SET NAME = ?, PASSWORD = ? where EMAIL = ?",
+			member.getName(), member.getPassword(), member.getEmail()
+		);
 	}
 	
-	public Collection<Member> selectAll() {
-		return null;
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query(
+				"select * from member",
+				(ResultSet rs, int rowNum) -> {
+					Member member = new Member(
+							rs.getString("EMAIL"),
+							rs.getString("PASSWORD"),
+							rs.getString("NAME"),
+							rs.getTimestamp("REGDATE").toLocalDateTime());
+					member.setId(rs.getLong("ID"));
+					return member;
+				});
+		return results;
+	}
+	
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject(
+			"select count(*) from member",Integer.class
+		);
+		return count;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
