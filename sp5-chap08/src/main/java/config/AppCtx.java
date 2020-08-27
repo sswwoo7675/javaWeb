dcacf72dbd3f02e838b8eb31bee0656dd8e03d2a
프로젝트 공간 <mu_2375@naver.com>
2020-08-27 15:44:05 +0900

    0827
    0827

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -7,7 +7,12 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import spring.ChangePasswordService;
 import spring.MemberDAO;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
 
 @Configuration
 @EnableTransactionManagement
@@ -28,6 +33,7 @@ public class AppCtx {
 		return ds;
 	}
 	
+	@Bean
 	public PlatformTransactionManager transactionManager() {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
 		tm.setDataSource(dataSource());
@@ -37,5 +43,35 @@ public class AppCtx {
 	@Bean
 	public MemberDAO memberDao() {
 		return new MemberDAO(dataSource());
+	}
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao());
+		return pwdSvc;
+	}
+	
+	@Bean
+	public MemberPrinter memberPrinter() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter(memberDao(), memberPrinter());
+	}
+	
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+		infoPrinter.setMemDao(memberDao());
+		infoPrinter.setPrinter(memberPrinter());
+		return infoPrinter;
 	}
 }
